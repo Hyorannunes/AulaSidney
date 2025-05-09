cd3e67269bc249022def91d4f2548c8e7ed225f9
Hyorannunes <hyoran2000@gmail.com>
2025-05-09 01:03:40 -0300

    feat: adicinonada nova linha de orientacao

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
-// neste repositorio vou aidicionar os codigos realizados na aula do sidney 
+// neste repositorio vou adicionar os codigos realizados na aula do sidney 
 // de uma maneira errada, porem para criar o costuma de usar o github todo dia
 // usando uma branch para armazenar cada codigo realizado em aula
+// por esse motivo nao usarei a branch homolog, todos seram armazenas e dados push sempre em uma unica branch
