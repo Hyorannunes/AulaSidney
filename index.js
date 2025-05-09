78eb712046fe279f21b032c3e2ab71bf5b88a952
Hyorannunes <hyoran2000@gmail.com>
2025-05-09 01:01:25 -0300

    firts commit

---
+++ index.js
@@ -0,0 +1,3 @@
+// neste repositorio vou aidicionar os codigos realizados na aula do sidney 
+// de uma maneira errada, porem para criar o costuma de usar o github todo dia
+// usando uma branch para armazenar cada codigo realizado em aula
