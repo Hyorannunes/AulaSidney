98e85873a7d580f6d2beba1876dffaa373b7793c
Hyoran <hyoran2000@gmail.com>
2025-05-12 20:19:06 -0300

    feat: create package Controller and UsarioController

---
+++ src/main/java/com/cadastro/controller/UsuarioController.java
@@ -0,0 +1,15 @@
+package com.cadastro.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/usuario")
+public class UsuarioController {
+    
+    @PostMapping
+    public void salvar(){
+
+    }
+}
